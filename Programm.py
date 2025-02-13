cc17205f2a3c38eb3e1a6776e6ee5320a88d53fd
MrsDoerte <jostsop@googlemail.com>
2025-02-13 11:41:46 +0100

    Sophies Code Steht, funktioniert aber nicht ideal

--- Programm.py
+++ Programm.py
@@ -7,27 +7,48 @@
 # Loop Abfrage Nochmal? Y/N
 
 # Danke Ende
+import os
 
 abfrage = True
 
-while(abfrage == True):
-    print("Text")
-    #Input
-    match(input):
-        case "1":
-            #Funktion
-            BarbarasFunktion()
-            abfrage
-            break
-        case "2":
-            ...
-
-abfrage2 = True
-while(abfrage2 == True):
-    #Nochmal?? If/else
-    break
-
-#Ende Tschüss
-
 def BarbarasFunktion():
-    #Barbaras Code
+    return
+
+def SophiesFunktion():
+    print("Sophie war hier")
+
+while(abfrage == True):
+    print("MENÜ\n________________________\n")
+    print("Bitte wähle:\n1 für Denise, 2 für Barbara, 3 für Sophie, 8 für Exit")
+    eingabe = input()
+    match(eingabe):
+        case "1":
+        case "2":
+        case "3":
+            SophiesFunktion()
+        case "8":
+            print("Verlassen wurde eingeleitet")
+            abfrage = False
+            break
+        case _:
+            print("Antwort nicht erkannt, bitte erneut wählen:\n")
+            abfrage = True
+            continue         
+    abfrage2 = True
+    print("Noch eine Runde? Ja oder Nein")
+    eingabe = input().strip().lower()
+    while(abfrage2 == True):
+        if eingabe == "ja":
+            abfrage2 = False
+            abfrage = True
+        elif eingabe == "nein":
+            print("Alles klar, noch eine Runde")
+            abfrage = False
+            abfrage2 = False
+        else:
+            print("Error, Tschüssi")
+            abfrage = False
+            abfrage2 = False
+
+print("Danke, tüdeldüh")
+#Ende Tschüss
