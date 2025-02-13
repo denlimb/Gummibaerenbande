67294bfdc9becafa02f3b511c0dff040cd6c29f4
Niennanarya <niennanarya@googlemail.com>
2025-02-13 10:46:00 +0000

    BarbaraFunktion

--- Programm.py
+++ Programm.py
@@ -7,20 +7,35 @@
 # Loop Abfrage Nochmal? Y/N
 
 # Danke Ende
+import time
+
+def BarbarasFunktion():
+    time.sleep(1)
+    print("\33[31m->   :\\33[0m")
+    time.sleep(1)
+    print("\33[32m->  :)\33[0m")
+    time.sleep(1)
+    print("\33[34m-> :D\33[0m")
+    #Barbaras Code
 
 abfrage = True
 
 while(abfrage == True):
-    print("Text")
+    print("Bitte wählen:\n1 für Denise \n2 für Barbara \n3 für Sophie \n8 für Exit")
+    eingabe = input()
     #Input
-    match(input):
+    match(eingabe):
         case "1":
             #Funktion
-            BarbarasFunktion()
             abfrage
             break
         case "2":
-            ...
+            BarbarasFunktion()
+            abfrage
+            break
+        case "3":
+
+            abfrage = False
 
 abfrage2 = True
 while(abfrage2 == True):
@@ -28,6 +43,3 @@ while(abfrage2 == True):
     break
 
 #Ende Tschüss
-
-def BarbarasFunktion():
-    #Barbaras Code
