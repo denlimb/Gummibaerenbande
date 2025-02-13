41d24d58a5c182f31cbc6aec3f825e4a862208ef
MrsDoerte <jostsop@googlemail.com>
2025-02-13 10:40:30 +0100

    Grundstruktur

--- Programm.py
+++ Programm.py
@@ -1,5 +1,3 @@
-# Hallo, Hier bin ich, alle Kinder kennen mich!
-
 #Grundmenü 1, 2, 3, 8-Exit
 
 # match Abfrage
@@ -9,3 +7,27 @@
 # Loop Abfrage Nochmal? Y/N
 
 # Danke Ende
+
+abfrage = True
+
+while(abfrage == True):
+    print("Text")
+    #Input
+    match(input):
+        case "1":
+            #Funktion
+            BarbarasFunktion()
+            abfrage
+            break
+        case "2":
+            ...
+
+abfrage2 = True
+while(abfrage2 == True):
+    #Nochmal?? If/else
+    break
+
+#Ende Tschüss
+
+def BarbarasFunktion():
+    #Barbaras Code
